b93cb8ac1521fbc783f72cd37285081595c4e4c8
Matthew Burke <matthelb@usc.edu>
2015-06-28 06:15:34 -0700

    Afdhdfgfd

--- php/api.php
+++ php/api.php
@@ -2,14 +2,44 @@
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/functions.php';
 
+function getFacebookCookie() {
+  $app_id = 'facebook_app_id';
+  $application_secret = 'facebook_app_secret';
+  if (isset($_COOKIE['fbsr_' . $app_id])) {
+    list($encoded_sig, $payload) = explode('.', $_COOKIE['fbsr_' . $app_id], 2);
+    $sig = base64_decode(strtr($encoded_sig, '-_', '+/'));
+    $data = json_decode(base64_decode(strtr($payload, '-_', '+/')), true);
+    if (strtoupper($data['algorithm']) !== 'HMAC-SHA256') {
+      return null;
+    }
+    $expected_sig = hash_hmac('sha256', $payload, $application_secret,
+      $raw = true);
+    if ($sig !== $expected_sig) {
+      return null;
+    }
+    $token_url = "https://graph.facebook.com/oauth/access_token?client_id="
+      . $app_id . "&client_secret=" . $application_secret. "&redirect_uri="
+      . "&code=" . $data['code'];
+ 
+    $response = @file_get_contents($token_url);
+    $params = null;
+    parse_str($response, $params);
+    $data['access_token'] = $params['access_token'];
+    echo $params;
+    return $data;
+  }else{
+    return null;
+  }
+}
+
+$user = getUser(getFacebookCookie());
+
 $app = new \Slim\Slim();
 
 $app->post('/groups/', function() {
-  
 });
 
 $app->get('/groups/:id', function($id) {
-  return json_encode(array(GetGroups($id)));
 });
 
 $app->put('/groups/:id', function($id) {
@@ -19,18 +49,36 @@ $app->delete('/groups/:id', function($id) {
 });
 
 $app->get('/groups/:id/transactions', function($id) {
+
 });
 
 $app->post('/transactions/', function() {
+  return json_encode(array(
+    'group_transaction' => createTransaction($user,
+      $app->request->params('group_id'),
+      $app->request->params('description'),
+      json_decode($app->request->params('transaction_map')))
+    ));
 });
 
 $app->put('/transactions/:id', function($id) {
+  return json_encode(array(
+    'group_transaction' => editTransaction($user, $id,
+      $app->request->params('description'),
+      json_decode($app->request->params('transaction_map')))
+  ));
 });
 
 $app->delete('/transaction/:id', function($id) {
+  return json_encode(array(
+    'success' => deleteTransaction($user, $id)
+  ));
 });
 
 $app->get('/users/:id/transactions', function($id) {
+  return json_encode(array(
+    'user_transactions' => getUserTransactionsWith($user, $id)
+  ));
 });
 
 $app->run();
--- php/functions.php
+++ php/functions.php
@@ -7,4 +7,81 @@ function GetGroups($id) {
   $stmt->bindValue(':id', $id, SQLITE3_TEXT);
   return $stmt->execute();
 }
+
+
+
+
+
+
+function createTransaction($user, $group_id, $purpose, $transaction_map) {
+  $db = new SQLite3('development.sqlite3');
+  $stmt = $db->prepare('INSERT INTO group_transactions (user_id, group_id,
+    purpose) VALUES (:user_id, :group_id, :purpose);');
+  $stmt->bindValue('user_id', $user['id'], SQLITE3_INTEGER);
+  $stmt->bindValue('group_id', $group_id, SQLITE3_INTEGER);
+  $stmt->bindValue('purpose', $purpose, SQLITE3_TEXT);
+  $result = $stmt->execute();
+  if ($result) {
+    $group_transaction_id = $db->lastInsertRowID();
+    foreach ($transaction_map as $user_id => $amount) {
+      $stmt = $db->prepare('INSERT INTO user_transactions (user_id,
+        other_user_id, group_transaction_id, amount VALUES (:user_id,
+        :other_user_id, :group_transaction_id, :amount);');
+      $stmt->bindValue('user_id', $user['id'], SQLITE3_INTEGER);
+      $stmt->bindValue('other_user_id', $user_id, SQLITE3_INTEGER);
+      $stmt->bindValue('group_transaction_id', $group_transaction_id,
+        SQLITE3_INTEGER);
+      $stmt->bindValue('amount', $amount, SQLITE3_INTEGER);
+      $result = $stmt->execute();
+      if (!$result) {
+        return null;
+      }
+    }
+    return array(
+      'user_id' => $user['id'],
+      'group_id' => $group_id,
+      'purpose' => $purpose
+    );
+  } else {
+    return null;
+  }
+}
+
+function editTransaction($user, $transaction_id, $transaction_id) {
+}
+
+function deleteTransaction($user, $transaction_id) {
+}
+
+function getUserTransactionsWith($user, $other_user_id) {
+  $query = $other_user_id ? 
+    'SELECT * FROM user_transactions WHERE user_id = :user_id AND
+      other_user_id = :other_user_id OR user_id = :other_user_id
+      AND other_user_id = :user_id' :
+    'SELECT * FROM user_transactions WHERE user_id = :user_id OR
+      other_user_id = :user_id';
+  $db = new SQLite3('development.sqlite3');
+  $stmt = $db->prepare($query);
+  $stmt->bindValue('user_id', $user['id'], SQLITE3_INTEGER);
+  if ($other_user_id) {
+    $stmt->bindValue('other_user_id', $other_user_id, SQLITE3_INTEGER);
+  }
+  $result = $stmt->execute();
+  if ($result) {
+    $res_arr = array();
+    while ($row = $result->fetchArray()) {
+      array_push($resarr, $row);
+    }
+    return $res_arr;
+  } else {
+    return null;
+  }
+
+}
+
+function getUser($facebook_hash) {
+  $db = new SQLite3('development.sqlite3');
+  $stmt = $db->prepare('SELECT * FROM users WHERE fb_user_id = :fb_id;');
+  $stmt->bindValue('
+}
 ?>
