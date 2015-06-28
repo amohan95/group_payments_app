ed6e37e8646a8c01087afea746bbe51dafaf53a6
Ananth Mohan <ananthmo@usc.edu>
2015-06-27 20:14:39 -0700

    Fixed merge conflicts

--- client/js/main.js
+++ client/js/main.js
@@ -1,1 +1,9 @@
+$(document).ready(function() {
+  var el = new UserCard("Ananth Mohan", "./../img/ananth-profile.jpg", 500);
+  $("#user-page").append(el);
+  $("#user-page").append(new UserCard("Bananth Bmohan", "./../img/ananth-profile.jpg", 25));
 
+  $("#group-page").append(new GroupCard("Stupidest Group", "./../img/ananth-profile.jpg",
+                                        50, 500, ["Bananthus", "Binuantoiwe", "Hitler", "AlsoAnanth",
+                                        "NotAnantyh"]));
+});
