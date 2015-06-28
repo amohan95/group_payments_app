ea49fec5c7f67c00276ccd823081c76029a9585b
Rochelle <rwillard@usc.edu>
2015-06-27 17:48:12 -0700

    syntax fix

--- app/controllers/api/v1/transactions.rb
+++ app/controllers/api/v1/transactions.rb
@@ -5,14 +5,14 @@ module API
       resource :transactions do
       	desc "Creates a new transaction."
       	params do
-      		requires :group_id, type: String, desc: "Group ID", , allow_blank: false
+      		requires :group_id, type: String, desc: "Group ID", allow_blank: false
       		requires :description, type: String, desc: "Transaction description", allow_blank: false
-    		  requires :transaction_map, type: Hash, desc: "Transaction map" do
+    		  requires :transaction_map, type: Hash, desc: "Transaction map", allow_blank: false do
 				    requires :user_id, type: String, desc: "User ID", allow_blank: false
 				    requires :amount, type: Integer, desc: "Charge amount", allow_blank: false
 				  end
       	end
-      	post do
+      	postg do
       		#stub
       	end
 
@@ -20,7 +20,7 @@ module API
       	route_param :id do
 	      	desc "Edits a transaction."
 	      	params do
-	      		requires :transaction_map, type: Hash, desc: "Transaction map" do
+	      		requires :transaction_map, type: Hash, desc: "Transaction map", allow_blank: false do
 				    	requires :user_id, type: String, desc: "User ID", allow_blank: false
 				    	requires :amount, type: Integer, desc: "Charge amount", allow_blank: false
 				  	end
