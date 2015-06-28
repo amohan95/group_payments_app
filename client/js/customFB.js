90bde23ca394e467fe10386ae33cd038e617669d
Ananth Mohan <ananthmo@usc.edu>
2015-06-28 00:27:22 -0700

    Added events card, cleaned up layouts

--- client/js/customFB.js
+++ client/js/customFB.js
@@ -1,7 +1,5 @@
 // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
     // The response object is returned with a status field that lets the
     // app know the current login status of the person.
     // Full docs on the response object can be found in the documentation
@@ -13,7 +11,7 @@
       }
 
       if(document.title == "Home") {
-        updateHomeSuccessfulLogin();
+        updateHomeSuccessfulLogin(response);
       }
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
@@ -32,38 +30,34 @@
     });
   }
 
-  function updateHomeSuccessfulLogin() {
-
-    FB.api('/me', function(response) {
-        console.log(response);
-    });
+  function updateHomeSuccessfulLogin(response) {
+    $(document).trigger('fb-ready');
   }
 
   window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '439521226251101',
-    cookie     : true,  // enable cookies to allow the server to access 
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.3' // use version 2.2
-  });
+    FB.init({
+      appId      : '439521226251101',
+      cookie     : true,  // enable cookies to allow the server to access 
+                          // the session
+      xfbml      : true,  // parse social plugins on this page
+      version    : 'v2.3' // use version 2.2
+    });
 
-  // Now that we've initialized the JavaScript SDK, we call 
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
+    // Now that we've initialized the JavaScript SDK, we call 
+    // FB.getLoginStatus().  This function gets the state of the
+    // person visiting this page and can return one of three states to
+    // the callback you provide.  They can be:
+    //
+    // 1. Logged into your app ('connected')
+    // 2. Logged into Facebook, but not your app ('not_authorized')
+    // 3. Not logged into Facebook and can't tell if they are logged into
+    //    your app or not.
+    //
+    // These three cases are handled in the callback function.
 
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
   };
 
   // Load the SDK asynchronously
--- client/js/main.js
+++ client/js/main.js
@@ -1,9 +1,54 @@
-$(document).ready(function() {
-  var el = new UserCard("Ananth Mohan", "./../img/ananth-profile.jpg", 500);
-  $("#user-page").append(el);
-  $("#user-page").append(new UserCard("Bananth Bmohan", "./../img/ananth-profile.jpg", 25));
+$(document).ready(function() { });
 
-  $("#group-page").append(new GroupCard("Stupidest Group", "./../img/ananth-profile.jpg",
+$(document).on('fb-ready', function(response) {
+  FB.api("/me/picture", function(response) {
+
+  });
+});
+
+function createStupidThings() {
+  var el = new createNewUserCard("Ananth Mohan", "./../img/ananth-profile.jpg", -500);
+  $("#user-page").append(el);
+  $("#user-page").append(createNewUserCard("Bananth Bmohan", "./../img/ananth-profile.jpg", 25));
+
+  $("#group-page").append(createNewGroupCard("Stupidest Group", "./../img/ananth-profile.jpg",
                                         50, 500, ["Bananthus", "Binuantoiwe", "Hitler", "AlsoAnanth",
                                         "NotAnantyh"]));
-});
+
+  $("#event-page").append(createNewEventCard("Stupid Event", true));  
+}
+
+function createNewEventCard(event_name, has_unresolved_debts) {
+  return new EventCard(event_name, has_unresolved_debts);
+}
+
+function createNewGroupCard(name, img_url, total_owed_to, total_owed_from, group_members) {
+  return new GroupCard(name, img_url, convertAmount(total_owed_to),
+                       convertAmount(total_owed_from), group_members);
+}
+
+function createNewUserCard(name, img_url, transaction_amount, description) {
+  if(!description) description = ""; 
+  return new UserCard(name, img_url, convertAmount(transaction_amount),
+                      transaction_amount < 0, description);
+}
+
+function convertAmount(cents) {
+  if(cents < 0) cents *= -1;
+  var amount = parseFloat(cents) ? cents : 0.0;
+
+  var components = (amount / 100).toFixed(2).toString().split(".");
+  var decimal = components[1];
+  var dollars = components[0] || "";
+  var mod, remainder;
+  var stringReverse = function(str) {
+      return str.split("").reverse().join("");
+  };
+
+  if(dollars.length > 3) {
+      dollars = stringReverse(dollars);
+      remainder = (mod = dollars.length % 3) ? stringReverse(dollars.slice(mod * -1)) + "," : "";
+      dollars = remainder + stringReverse(dollars.match(/.../g).join(","));
+  }
+  return ["$", [dollars, decimal].join(".")].join("");
+}
