cf22791205f21200ef1f808f5f83325c8dab0484
Rochelle <rwillard@usc.edu>
2015-06-27 21:46:24 -0700

    test db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627230459) do
+ActiveRecord::Schema.define(version: 20150628041130) do
 
   create_table "group_transactions", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -33,7 +33,12 @@ ActiveRecord::Schema.define(version: 20150627230459) do
   create_table "user_groups", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "group_id"
   end
+
+  add_index "user_groups", ["group_id"], name: "index_user_groups_on_group_id"
+  add_index "user_groups", ["user_id"], name: "index_user_groups_on_user_id"
 
   create_table "user_transactions", force: :cascade do |t|
     t.datetime "updated_at",           null: false
