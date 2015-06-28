30c09c23d516fa3a17f30d1ffcb0a1394e4cf4d4
Matthew Burke <matthelb@usc.edu>
2015-06-28 00:57:47 -0700

    Add find_or_create_by_auth_hash to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   has_many :other_user_transactions, source: :other_user
   has_many :user_groups
   has_many :groups, through: :user_groups
+
+  def self.find_or_create_from_auth_hash(auth_hash)
+    User.find_or_create_by(fb_user_id: auth_hash['uid'])
+  end
+
 end
